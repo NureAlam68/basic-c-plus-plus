b9b2889676c09ffb8e1e377dcea0d321360ebdca
NureAlam68 <nurealam151068@gmail.com>
2025-06-29 17:15:26 +0600

    print

---
+++ print.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main()
+{
+    int x = 100;
+    char c = 'A';
+    double d = 2.34;
+
+    std::cout << x << " " << c << std::endl << d << std::endl;
+    std::cout << "My favorite number is " << x << std::endl;
+    return 0;
+}
