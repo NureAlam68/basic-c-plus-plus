b0b7e6967a4f804e1331009e30a163ef5226045e
NureAlam68 <nurealam151068@gmail.com>
2025-06-29 21:45:02 +0600

    string

---
+++ string.cpp
@@ -0,0 +1,34 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     char s[100];
+//     cin.getline(s, 100);
+//     cout << s << endl;
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cin >> x;
+//     cin.ignore(); // Clear the newline character from the input buffer
+//     char s[100];
+//     cin.getline(s, 100);
+//     cout << x << endl << s << endl;
+//     return 0;
+// }
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    cout << s << endl;
+    return 0;
+}
